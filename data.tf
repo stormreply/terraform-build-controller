1f13fbf0cd28e7d5b01be8afad8099fc4aaba24b
Bernd Herding <bernd.herding@external.biontech.com>
2025-08-25 11:09:28 +0200

    update default ami search pattern

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "latest_amazon_linux_ami" {
   owners      = ["amazon"]
   filter {
     name   = "name"
-    values = ["al2023-ami-*"]
+    values = ["al2023-ami-2023*"]
     # e.g. al2023-ami-2023.3.20231218.0-kernel-6.1-x86_64
   }
   filter {
