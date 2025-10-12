11036d22589f0aaff029594e51b46f2be15c5922
Bernd Herding <bernd.herding@external.biontech.com>
2025-10-12 22:19:29 +0200

    use current region as default

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   ami           = var.ami != null ? var.ami : data.aws_ami.latest_amazon_linux_ami.id
   instance_name = var.instance_name == null ? var.deployment.name : var.instance_name
-  region        = data.aws_region.current.region
+  region        = var.region != null ? var.region : data.aws_region.current.region
 }
